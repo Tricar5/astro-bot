f66a29a0ba9d7efb2077a74e9d511505444696f6
a.kazachinskiy <kazachinskiy.a@romir.ru>
2023-05-03 22:19:47 +0300

    :tada: initial commit

---
+++ bot/__main__.py
@@ -0,0 +1,18 @@
+
+
+
+async def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode="HTML")
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
